3c36e89faed1a919b6885b5bcd7b4310cd4cdbb9
Massimo Del Pezzo <massimo.dpezzo@gmail.com>
2023-05-01 20:22:29 +0800

    Config

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:atomsbox/atomsbox.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: AppTheme.darkTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
